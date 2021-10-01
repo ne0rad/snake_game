190915befe85713289a7837b2a43bfe5a242365d
ne0rad <usackasr@gmail.com>
2021-10-01 14:59:50 +0100

    Added wall collision

--- snake_game.py
+++ snake_game.py
@@ -8,8 +8,8 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
-SQUARE_SIZE = 20  # This sets the size of each square in the grid
-GRID_SIZE = 30  # Set grid size (n * n squares)
+SQUARE_SIZE = 18  # This sets the size of each square in the grid
+GRID_SIZE = 40  # Set grid size (n * n squares)
 MARGIN = 1
 
 SIZE = MARGIN*2 + (GRID_SIZE*SQUARE_SIZE+MARGIN*GRID_SIZE)
@@ -59,8 +59,7 @@ grid[tail_loc[0]][tail_loc[1]] = 1
 
 
 def move_tail():
-    print(tail_dir)
-    print(tail_loc)
+    # Removes tail square and sets new position for tail
     grid[tail_loc[0]][tail_loc[1]] = 0
     if tail_dir[-1] == 'up':
         tail_loc[0] -= 1
@@ -74,6 +73,22 @@ def move_tail():
     return
 
 
+def set_dir(direction):
+    global head_dir
+    global move_clock
+    head_dir = direction
+    move_tail()
+    tail_dir.insert(0, direction)
+    move_clock = 0
+    return
+
+def check_collision():
+    global done
+    if(head_loc[0] <= 0 or head_loc[1] <= 0 or head_loc[0] >= GRID_SIZE-1 or head_loc[1] >= GRID_SIZE-1):
+        print("COLLISION")
+        done = True
+    return
+
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get():  # User did something
@@ -83,40 +98,29 @@ while not done:
 
             if event.key == pygame.K_ESCAPE:  # Quit if ESC key pressed
                 done = True
-            if event.key == pygame.K_w:
+            elif event.key == pygame.K_w:
                 if(head_dir != 'up' and head_dir != 'down'):
                     grid[head_loc[0] - 1][head_loc[1]] = 1
-                    head_dir = 'up'
-                    move_tail()
-                    tail_dir.insert(0, 'up')
+                    set_dir('up')
                     head_loc[0] -= 1
-                    move_clock = 0
-            if event.key == pygame.K_d:
+            elif event.key == pygame.K_d:
                 if(head_dir != 'right' and head_dir != 'left'):
                     grid[head_loc[0]][head_loc[1] + 1] = 1
-                    head_dir = 'right'
-                    move_tail()
-                    tail_dir.insert(0, 'right')
+                    set_dir('right')
                     head_loc[1] += 1
-                    move_clock = 0
-            if event.key == pygame.K_s:
+            elif event.key == pygame.K_s:
                 if(head_dir != 'down' and head_dir != 'up'):
                     grid[head_loc[0] + 1][head_loc[1]] = 1
-                    head_dir = 'down'
-                    move_tail()
-                    tail_dir.insert(0, 'down')
+                    set_dir('down')
                     head_loc[0] += 1
-                    move_clock = 0
-            if event.key == pygame.K_a:
+            elif event.key == pygame.K_a:
                 if(head_dir != 'left' and head_dir != 'right'):
                     grid[head_loc[0]][head_loc[1] - 1] = 1
-                    head_dir = 'left'
-                    move_tail()
-                    tail_dir.insert(0, 'left')
+                    set_dir('left')
                     head_loc[1] -= 1
-                    move_clock = 0
 
-    if move_clock <= speed:
+    check_collision()
+    if move_clock < speed:
         move_clock += 1
     else:
         if(head_dir == 'left'):
@@ -125,20 +129,20 @@ while not done:
             tail_dir.insert(0, 'left')
             head_loc[1] -= 1
 
-        if(head_dir == 'down'):
+        elif(head_dir == 'down'):
             grid[head_loc[0] + 1][head_loc[1]] = 1
             move_tail()
             tail_dir.insert(0, 'down')
             head_loc[0] += 1
 
-        if(head_dir == 'right'):
+        elif(head_dir == 'right'):
             grid[head_loc[0]][head_loc[1] + 1] = 1
             head_dir = 'right'
             move_tail()
             tail_dir.insert(0, 'right')
             head_loc[1] += 1
 
-        if(head_dir == 'up'):
+        elif(head_dir == 'up'):
             grid[head_loc[0] - 1][head_loc[1]] = 1
             head_dir = 'up'
             move_tail()
